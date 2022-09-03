5e38a0820a3122a99888184d48e821149c292746
Edvan Trindade <trindade3e@gmail.com>
2022-09-03 19:22:34 -0300

    Finished exercise 3.8

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('postContent', (request, response) =>
+  JSON.stringify(request.body)
+);
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      tokens.postContent(req, res),
+    ].join(' ');
+  })
+);
 
 const PORT = 3001;
 
